7eb44a330647c18dfb0dc4b4fb6c01cb25d05d04
Bardita Andrei <bmihaiandrei@gmail.com>
2023-01-17 18:32:21 +0200

    orders analytics

--- src/routes/analytics.ts
+++ src/routes/analytics.ts
@@ -4,5 +4,6 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/analytics", Analytics.readAll);
+router.get("/analytics/orders", Analytics.readOrders);
 
 export default router;
